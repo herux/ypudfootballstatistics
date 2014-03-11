b5475ea739f85eaf5eb53b744317f636129d2236
herux <heruxi@gmail.com>
2014-03-11 21:03:22 +0700

    first commit of webservices
    first commit of webservices

---
+++ webservices/ypudaction.pas
@@ -0,0 +1,26 @@
+unit ypudaction;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  BrookAction;
+
+type
+  TYPUDAction = class(TBrookAction)
+  public
+    procedure Get; override;
+  end;
+
+implementation
+
+procedure TYPUDAction.Get;
+begin
+  Write('Your content here ...');
+end;
+
+initialization
+  TYPUDAction.Register('*');
+
+end.
---
+++ webservices/ypudfs.lpr
@@ -0,0 +1,10 @@
+program ypudfs;
+
+{$mode objfpc}{$H+}
+
+uses
+  BrookApplication, ypudbrokers, ypudaction;
+
+begin
+  BrookApp.Run;
+end.
